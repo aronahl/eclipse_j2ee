8b8738b046335ebc5c97d864d84b472bdcb390c3
aronahl <aronahl@users.noreply.github.co>
2017-05-07 19:10:15 -0400

    Run sha512 check on eclipse download.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # eclipse_j2ee
 A Docker build of __Eclipse for Java 2 EE Developers__ accessible via [xpra](https://xpra.org/).
 
+
+
 ##Recommended Usage
 1. Start the container.
 
 	```bash
-	$ docker run --rm -it -p 9999:9999 -v eclipse:/opt/eclipse -v ~/workspace:/opt/workspace eclipse
+	$ docker run --rm -it -p 9999:9999 -v eclipse:/opt/eclipse -v ~/workspace:/opt/workspace aronahl/eclipse
 	```
 	* The eclipse install will be copied to the eclipse volume.
 		* If no eclipse volume exists, it will be created and filled with a copy of eclipse from the image.
@@ -14,5 +16,4 @@ A Docker build of __Eclipse for Java 2 EE Developers__ accessible via [xpra](htt
 
 2. Attach with an xpra client to tcp:localhost:9999
 
-##Caution
-The installation of eclipse is a scripted download which does not validate the checksum of the archive.  A compromised mirror could deliver a compromised eclipse install.
+
--- getUrl.py
+++ getUrl.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import urllib.request, urllib.error, urllib.parse
 from bs4 import BeautifulSoup
-import shutil
 import sys
 import re
 import os
+import hashlib
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://www.eclipse.org/downloads/eclipse-packages/?osType=linux&release=undefined') as f:
@@ -14,10 +14,21 @@ if __name__ == "__main__":
         b64 = 'https://www.eclipse.org' + b64
     with urllib.request.urlopen(b64) as f:
         page = BeautifulSoup(f.read(), 'html.parser')
-        link = page.find(href = re.compile('^download\.php.+'))['href']
-        link = 'https://www.eclipse.org/downloads/' + link
-    with urllib.request.urlopen(link) as f:
-        page = BeautifulSoup(f.read(), 'html.parser')
-        finalLink = page.find(lambda tag: tag.name == "a" and tag.string is not None and tag.string.strip() == "click here")["href"]
-        with urllib.request.urlopen(finalLink) as f, os.fdopen(sys.stdout.fileno(), 'wb') as stdout:
-            shutil.copyfileobj(f, stdout)
+        finalLink = page.find(lambda tag: tag.name == "a" and tag.string is not None and tag.string.strip().lower() == "direct link to file")["href"]
+        finalLink = 'https://www.eclipse.org/downloads/' + finalLink
+        shaLink = finalLink.replace('download.php', 'sums.php')
+    with urllib.request.urlopen(shaLink) as f:
+        shasum = bytes.fromhex(f.read().split()[0].decode('utf-8'))
+        #link = page.find(href = re.compile('^download\.php.+'))['href']
+        #link = 'https://www.eclipse.org/downloads/' + link
+    #with urllib.request.urlopen(link) as f:
+    #    page = BeautifulSoup(f.read(), 'html.parser')
+    #    finalLink = page.find(lambda tag: tag.name == "a" and tag.string is not None and tag.string.strip() == "click here")["href"]
+    sha = hashlib.sha512()
+    with urllib.request.urlopen(finalLink) as f, os.fdopen(sys.stdout.fileno(), 'wb') as stdout:
+        for part in iter(lambda: f.read(8192), b""):
+            sha.update(part)
+            stdout.write(part)
+        if sha.digest() != shasum:
+            stdout.write('sha512 check failed'.encode('utf-8'))
+            raise Exception('sha512 check failed')
