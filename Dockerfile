bdf545b8e6bca295f85b6deea10b01f0be281d21
Aaron Nall <aronahl@hotmail.com>
2019-04-25 15:44:00 -0400

    Fix compatibility issues with latest xpra and use --start for delayed start instead of checking log file.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM ubuntu:16.04
 ENV DEBIAN_FRONTEND=noninteractive
-ADD ["getUrl.py", "runit.py", "/usr/local/bin/"]
 WORKDIR /opt
 RUN apt-get update && \
     apt-get upgrade -fy && \
@@ -12,14 +11,15 @@ RUN apt-get update && \
             openjdk-8-source \
             software-properties-common \
             greybird-gtk-theme && \
-    pip3 install bs4 dumb-init && \
-    /usr/local/bin/getUrl.py | tar -xzv && \
+    pip3 install bs4 dumb-init
+ADD ["getUrl.py", "runit.py", "/usr/local/bin/"]
+RUN /usr/local/bin/getUrl.py | tar -xzv && \
     curl http://winswitch.org/gpg.asc | apt-key add - && \
     echo "deb http://winswitch.org/ xenial main" > /etc/apt/sources.list.d/winswitch.list && \
     add-apt-repository universe  && \
     apt-get update && \
     apt-get install -fy xpra && \
-    adduser user && \
+    adduser user --home /home/user && \
     mkdir /opt/workspace && \
     chown -R user /opt/eclipse /opt/workspace && \
     apt-get remove -y python3-pip curl && \
@@ -28,7 +28,11 @@ RUN apt-get update && \
     apt-get autoremove -y && \
     rm -rf /var/lib/apt/lists/* && \
     groupadd -g 999 docker && \
-    usermod -aG 999 user
+    usermod -aG 999 user && \
+    mkdir /run/user/1000 && \
+    chown user /run/user/1000
 USER user
+EXPOSE 9999
+VOLUME /opt/eclipse /opt/workspace /home/user /run/user
 ENTRYPOINT ["/usr/local/bin/dumb-init", "--", "/usr/local/bin/runit.py" ]
 CMD [ ]
--- runit.py
+++ runit.py
@@ -3,46 +3,45 @@ import subprocess
 import sys
 import os.path
 import os
+import stat
 import time
+import textwrap
 
 if __name__ == "__main__":
     if os.path.exists('/tmp/.X10-lock'):
         os.unlink('/tmp/.X10-lock')
-    if os.path.exists("/home/user/.xpra/:10.log"):
-        os.unlink("/home/user/.xpra/:10.log")
+    if os.path.exists("/run/user/1000/xpra/:10.log"):
+        os.unlink("/run/user/1000/xpra/:10.log")
     http = int(os.getenv("HTTP", "0"))
     xpraArgs = [
         "xpra",
         "--bind-tcp=0.0.0.0:9999",
         "--dpi=%s" % os.getenv("DPI", "92"),
         "start",
+        "--start",
+        "/tmp/launch.sh",
         ":10"]
+    oldmask = os.umask(0o077)
+    with open("/tmp/launch.sh", 'w') as f:
+        f.write(textwrap.dedent("""\
+                #!/bin/bash
+                exec /opt/eclipse/eclipse -data /opt/workspace
+        """))
+    os.chmod('/tmp/launch.sh', os.stat('/tmp/launch.sh').st_mode | stat.S_IEXEC)
+    os.umask(oldmask)
     if http:
         xpraArgs.insert(1, "--html=on")
+    subprocess.check_call(args=("whoami"))
+    subprocess.check_call(args=("grep", "user", "/etc/passwd"))
+    print(xpraArgs)
     subprocess.check_call(args=xpraArgs)
     logFound = False
     for _ in range(30):
-        if os.path.exists("/home/user/.xpra/:10.log"):
+        if os.path.exists("/run/user/1000/xpra/:10.log"):
             logFound = True
             break
         time.sleep(1)
     if not logFound:
         print("Startup failed without a log file")
         sys.exit(1)
-    tailProc = subprocess.Popen(args=("tail", "-f", "/home/user/.xpra/:10.log"))
-    eclipseArgs=["/opt/eclipse/eclipse", "-data", "/opt/workspace" ] + sys.argv[1:]
-    while not os.path.exists('/home/user/.xpra/:10.log'):
-        print('Waiting on log file.')
-        time.sleep(1)
-    #waiting for the connection before starting eclipse prevents odd DPI issues.
-    while True:
-        with open('/home/user/.xpra/:10.log', 'r') as f:
-            print('Waiting on TCP Connection.')
-            if "New tcp connection" in f.read():
-                break
-            time.sleep(1)
-    eclipseEnv = dict(os.environ)
-    eclipseEnv["DISPLAY"] = ":10"
-    eclipseProc = subprocess.Popen(args=eclipseArgs, env=eclipseEnv)
-    eclipseProc.wait()
-    tailProc.kill()
+    subprocess.check_call(args=("tail", "-f", "/run/user/1000/xpra/:10.log"))
